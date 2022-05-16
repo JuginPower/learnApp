9e4659cc5a4b97897a46c8d3df2057a1ee9d5cea
JuginPower <juginpower90@gmail.com>
2022-05-16 20:36:42 +0200

    making my tables sql

---
+++ flaskr/schema.sql
@@ -0,0 +1,41 @@
+
+DROP TABLE IF EXISTS user_learn;
+CREATE TABLE user_learn (
+  id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  benutzername TEXT NOT NULL,
+  passwort TEXT NOT NULL
+);
+
+INSERT INTO user_learn (id, benutzername, passwort) VALUES
+(1, 'jugin', 'balthazarkraft90');
+
+DROP TABLE IF EXISTS fach_learn;
+CREATE TABLE fach_learn (
+  id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  fach TEXT,
+  fach_id INTEGER,
+  FOREIGN KEY (fach_id) REFERENCES user_learn (id)
+);
+
+DROP TABLE IF EXISTS thema_learn;
+CREATE TABLE thema_learn (
+  id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  thema TEXT,
+  thema_id INTEGER,
+  thema_category INTEGER,
+  FOREIGN KEY (thema_id) REFERENCES user_learn (id),
+  FOREIGN KEY (thema_category) REFERENCES fach_learn (id)
+);
+
+DROP TABLE IF EXISTS quest_learn;
+CREATE TABLE quest_learn (
+  id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  frage text,
+  antwort text,
+  quest_id INTEGER,
+  fach_category INTEGER,
+  thema_category INTEGER,
+  FOREIGN KEY (quest_id) REFERENCES user_learn (id),
+  FOREIGN KEY (fach_category) REFERENCES fach_learn (id),
+  FOREIGN KEY (thema_category) REFERENCES thema_learn (id)
+);
